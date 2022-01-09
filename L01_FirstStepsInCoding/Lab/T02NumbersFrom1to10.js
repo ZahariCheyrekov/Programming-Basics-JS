cf4e27e637a0e846df2c49e7f2686d23641b38de
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-01-09 22:05:47 +0200

    Add files via upload

---
+++ L01_FirstStepsInCoding/Lab/T02NumbersFrom1to10.js
@@ -0,0 +1,5 @@
+function printNumbers (){
+    for (let i = 1; i <= 10; i++){
+        console.log(i);
+    }
+}
---
+++ L01_FirstStepsInCoding/Lab/T05GreetingByName.js
@@ -0,0 +1,4 @@
+function greet(input){
+    let name = input[0];
+    console.log(`Hello, ${name}!`);
+}
