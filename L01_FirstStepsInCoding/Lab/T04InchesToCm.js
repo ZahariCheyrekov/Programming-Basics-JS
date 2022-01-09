5a3ecf935a5bc53e8715bddfaf15766c98ca4d23
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-01-09 22:06:07 +0200

    Delete L01_FirstStepsInCoding/Lab directory

--- L01_FirstStepsInCoding/Lab/T04InchesToCm.js
+++
@@ -1,5 +0,0 @@
-function convert (input){
-    let inches = Number(input[0]);
-    let result = inches * 2.54;
-    console.log(result);
-}
--- L01_FirstStepsInCoding/Lab/T05GreetingByName.js
+++
@@ -1,4 +0,0 @@
-function greet(input){
-    let name = input[0];
-    console.log(`Hello, ${name}!`);
-}
