749b0be5e193842b0f1e32d60bea45696409148e
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-01-19 19:12:10 +0200

    Delete L05_WhileLoops/Lab directory

--- L05_WhileLoops/Lab/T01ReadText.js
+++
@@ -1,8 +0,0 @@
-function solve(input) {
-    let command = input.shift();
-
-    while (command !== "Stop") {
-        console.log(command);
-        command = input.shift();
-    }
-}
--- L05_WhileLoops/Lab/T04Sequence2k+1.js
+++
@@ -1,9 +0,0 @@
-function solve(input) {
-    const n = Number(input.shift());
-    let number = 1;
-
-    while (number <= n) {
-        console.log(number);
-        number = number * 2 + 1;
-    }
-}
