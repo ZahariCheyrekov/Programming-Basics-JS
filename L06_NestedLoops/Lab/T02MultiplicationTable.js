171e1eca34d3365b5863f9aaff41c7c0efa88fbb
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-01-23 10:47:06 +0200

    Delete L06_NestedLoops directory

--- L06_NestedLoops/Lab/T02MultiplicationTable.js
+++
@@ -1,8 +0,0 @@
-function solve(input) {
-    for (let x = 1; x <= 10; x++) {
-        for (let y = 1; y <= 10; y++) {
-            let product = x * y;
-            console.log(`${x} * ${y} = ${product}`);
-        }
-    }
-}
