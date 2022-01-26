cb33b8e3539b7648fb58db2effe4d0637592c865
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-01-26 12:29:48 +0200

    Delete L03_ConditionalStatementsAdvanced directory

--- L03_ConditionalStatementsAdvanced/Lab/T10InvalidNumber.js
+++
@@ -1,9 +0,0 @@
-function displayNumber(input) {
-    const number = Number(input[0]);
-
-    const isValid = number >= 100 && number <= 200 || number === 0;
-
-    if (!isValid) {
-        console.log("invalid");
-    }
-}
--- L03_ConditionalStatementsAdvanced/MoreExercises/T09NumbersFrom1To10.js
+++
@@ -1,7 +0,0 @@
-function numbersFrom1to10() {
-    let counter = 1;
-
-    while (counter <= 10) {
-        console.log(counter++);
-    }
-}
