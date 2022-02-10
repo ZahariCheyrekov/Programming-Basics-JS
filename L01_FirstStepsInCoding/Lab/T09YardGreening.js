ac9a0567febe619c99ad1d51e74143ca1ebb76f3
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-02-10 15:29:06 +0200

    Update T09YardGreening.js

--- L01_FirstStepsInCoding/Lab/T09YardGreening.js
+++ L01_FirstStepsInCoding/Lab/T09YardGreening.js
@@ -1,5 +1,6 @@
 function yardGreening(input){
     let meters = Number(input[0]);
+   
     let price = (meters * 7.61);
     let discount = (price * 0.18);
     let finalPrice = (price - discount);
