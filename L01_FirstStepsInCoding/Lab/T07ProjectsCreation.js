57fd3d350287b873013647a4c02d51db287d59a4
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-02-10 15:29:49 +0200

    Update T07ProjectsCreation.js

--- L01_FirstStepsInCoding/Lab/T07ProjectsCreation.js
+++ L01_FirstStepsInCoding/Lab/T07ProjectsCreation.js
@@ -1,7 +1,7 @@
 function createProjects(input){
     let name = input[0];
-   
- let projects = Number(input[1]);
+    let projects = Number(input[1]);
+    
     let hoursNeeded = projects * 3;
 
     console.log(`The architect ${name} will need ${hoursNeeded} hours to complete ${projects} project/s.`)
