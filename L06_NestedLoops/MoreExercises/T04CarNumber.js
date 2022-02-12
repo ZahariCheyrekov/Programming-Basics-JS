058e21387e2b17cc7a32c1f26bdaa69c043222b7
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-02-12 10:58:49 +0200

    Update T04CarNumber.js

--- L06_NestedLoops/MoreExercises/T04CarNumber.js
+++ L06_NestedLoops/MoreExercises/T04CarNumber.js
@@ -3,11 +3,14 @@ function solve(input) {
     const endInterval = Number(input.shift());
 
     let printLine = "";
+
     for (let i = startInterval; i <= endInterval; i++) {
         for (let j = startInterval; j <= endInterval; j++) {
             for (let k = startInterval; k <= endInterval; k++) {
                 for (let l = startInterval; l < i; l++) {
+    
                     if ((j + k) % 2 == 0) {
+                    
                         if (i % 2 == 0 && l % 2 == 1 || i % 2 == 1 && l % 2 == 0) {
                             printLine += "" + i + j + k + l + " ";
                         }
