f01d8d6bb74fc937f8d29aee6d8fabe449e1d60b
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-02-12 10:58:00 +0200

    Update T01UniquePinCode.js

--- L06_NestedLoops/MoreExercises/T01UniquePinCode.js
+++ L06_NestedLoops/MoreExercises/T01UniquePinCode.js
@@ -6,9 +6,11 @@ function solve(input) {
     for (let i = 2; i <= firstNumberCeil; i += 2) {
         for (let j = 2; j <= secondNumberCeil; j++) {
             for (let k = 2; k <= thirdNumberCeil; k += 2) {
+               
                 if (j == 2 || j == 3 || j == 5 || j == 7) {
+
                     console.log("" + i + " " + j + " " + k);
-                }
+               }
             }
         }
     }
